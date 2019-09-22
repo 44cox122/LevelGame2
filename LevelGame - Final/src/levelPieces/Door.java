cd43cf3f55cd8395703c197ba4ce2153ec0ef368
44cox122 <44152286+44cox122@users.noreply.github.com>
2019-09-22 16:51:50 -0600

    Add files via upload

---
+++ LevelGame - Final/src/levelPieces/Door.java
@@ -0,0 +1,29 @@
+package levelPieces;
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+
+//Door is our advance piece
+public class Door extends GamePiece implements Drawable{
+
+	public Door() {
+		//Constructor
+		super('D', GameEngine.BOARD_SIZE - 1);
+	}
+
+	
+	@Override
+	public InteractionResult interact(Drawable[] pieces, int playerLocation) {
+		//Telling what interaction to do
+		if(playerLocation == super.getLocation()) {return InteractionResult.ADVANCE;}
+		else {return InteractionResult.NONE;}
+	}
+	
+	
+	public void draw() {
+		//Draws the given char
+		System.out.print("D");
+	}
+	
+	
+}
